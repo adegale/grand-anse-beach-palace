227bec8910f1872aefea62ff625a2b2be90c379d
adegale <156728691+adegale@users.noreply.github.com>
2025-11-18 19:33:12 -0400

    Fix Rooms import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Navigation } from './components/Navigation';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
-import { Rooms } from './components/Rooms';
+import  Rooms  from './components/Rooms';
 import { Explore } from './components/Explore';
 import { Rates } from './components/Rates';
 import { Contact } from './components/Contact';
